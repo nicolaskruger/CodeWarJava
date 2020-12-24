01d4c1a9029dd833b8b0fe9ab9b18caf0d356dfd
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-12-24 16:15:31 -0300

    001

--- src/Kata.java
+++ src/Kata.java
@@ -2,13 +2,13 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Kata {
-    public static int squareSum(int[] n)
-    {
-        return Arrays.stream(n).reduce(0,(acc,cur)->acc+cur*cur);
+    public static boolean isSquare(int n) {
+        var num = Math.sqrt(n);
+        if(Double.isNaN(num)) return false;
+        return (num-((int)num))==0;
     }
-
     public static void main(String[] args) {
-        var n = Kata.squareSum(new int[]{1,2,3});
-        n++;
+        var n = Kata.isSquare(4);
+        n=false;
     }
 }
