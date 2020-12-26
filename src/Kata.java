bf0582727fd85f6fd920f6f7a455bf9b601fd1d0
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-12-25 22:55:48 -0300

    finish

--- src/Kata.java
+++ src/Kata.java
@@ -1,17 +1,20 @@
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Kata {
     public static int sortDesc(final int num) {
         //Your code
+        return
+        Integer.parseInt(
         Arrays.stream(Integer.toString(num)
                 .split(""))
                 .sorted()
+                .sorted(Collections.reverseOrder())
+                .reduce("",(acc,curr)->acc+curr)
+        );
+
     }
     public static void main(String[] args) {
 
