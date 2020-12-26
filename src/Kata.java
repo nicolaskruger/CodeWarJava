73787b19a20327c52fecf5c6dd7277580e2c4fdd
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-12-26 14:29:30 -0300

    001

--- src/Kata.java
+++ src/Kata.java
@@ -6,8 +6,9 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Kata {
-    public static boolean check(Object[] a, Object x) {
-        return Arrays.asList(a).contains(x);
+    public static String oddOrEven (int[] array) {
+        // your code
+        return Arrays.stream(array).sum()%2==0?"even":"odd";
     }
     public static void main(String[] args) {
 
