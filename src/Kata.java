1f00ee63320d864c3f4379b9f5dc2e8e3b68d633
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-12-26 20:12:58 -0300

    best

--- src/Kata.java
+++ src/Kata.java
@@ -10,24 +10,8 @@ import java.util.stream.Collectors;
 public class Kata {
     public static String order(String words) {
         return Arrays.stream(words.split(" "))
-                .sorted((a,b)->{
-                    Pattern pat = Pattern.compile("([0-9])");
-                    Matcher m1 = pat.matcher(a);
-                    Matcher m2 = pat.matcher(b);
-                    var v1 = "";
-                    if(m1.find()){
-                        v1 =(m1.group(0));
-                    }
-                    var v2 = "";
-                    if(m2.find()){
-                        v2 =(m2.group(0));
-                    }
-                    if (v1.charAt(0)<v2.charAt(0))
-                        return -1;
-                    if (v1.charAt(0)>v2.charAt(0))
-                        return +1;
-                    return 0;
-                }).collect(Collectors.joining(" "));
+                .sorted(Comparator.comparing(s -> Integer.valueOf(s.replaceAll("\\D", ""))))
+                .reduce((a, b) -> a + " " + b).get();
     }
 
 
