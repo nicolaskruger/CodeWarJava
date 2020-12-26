0c763a5b52af70c66adbf6ba5e2d068e860a4eb9
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-12-25 22:07:54 -0300

    best

--- src/Kata.java
+++ src/Kata.java
@@ -9,17 +9,11 @@ import java.util.stream.Collectors;
 public class Kata {
     public boolean check(String sentence){
         //code
-        return Arrays.stream(sentence.toLowerCase(Locale.ROOT)
-                .split(""))
-                .sorted()
-                .reduce("",(acc,curr)->{
-                    if(Pattern.compile("[a-z]").matcher(curr).find()){
-                        if(acc.length()==0||acc.charAt(acc.length()-1)!=curr.charAt(0))
-                            return acc+curr;
-                    }
-                    return acc;
-                })
-                .equals("abcdefghijklmnopqrstuvwxyz");
+        return sentence.chars()
+                .filter(Character::isLetter)
+                .map(Character::toLowerCase)
+                .distinct()
+                .count() == 26;
     }
     public static void main(String[] args) {
         var val = new Kata().check("The quick brown fox jumps over the lazy dog" );
