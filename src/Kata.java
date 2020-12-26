1c406ad065f500cbcc13541a96560c8b8232b3cd
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-12-26 16:22:31 -0300

    001

--- src/Kata.java
+++ src/Kata.java
@@ -6,12 +6,16 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Kata {
-    public static String oddOrEven (int[] array) {
-        // your code
-        return Arrays.stream(array).sum()%2==0?"even":"odd";
+    public int squareDigits(int n) {
+        // TODO Implement me
+        return
+        Integer.valueOf(Arrays.stream(Integer.toString(n)
+                .split(""))
+                .map(s->String.valueOf(Integer.valueOf(s)*Integer.valueOf(s)))
+                .collect(Collectors.joining()));
     }
     public static void main(String[] args) {
-
+        System.out.println(new Kata().squareDigits(9119));
     }
 
 
