753aca4af4e77c700f6b7fb567411ff772a4463c
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-12-31 21:17:42 -0300

    001

--- src/Kata.java
+++ src/Kata.java
@@ -9,59 +9,74 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Kata {
-    static Pattern upper = Pattern.compile("[A-Z]");
-    static Pattern lower = Pattern.compile("[a-z]");
-    public static List<String>  movingShift(String s, int shift) {
-        final int[] cont = {shift-1};
-		s = Arrays.stream(s.split(""))
-                .map(w->{
-                    cont[0]++;
-                    if (upper.matcher(w).find()){
-                        return String.valueOf((char) ((w.charAt(0)-'A'+cont[0])%('Z'-'A'+1)+'A'));
-                    }
-                    else if(lower.matcher(w).find()){
-                        return String.valueOf((char) ((w.charAt(0)-'a'+cont[0])%('z'-'a'+1)+'a'));
-                    }
-                   return w;
-                }).collect(Collectors.joining());
-		int div = (int) Math.ceil((double) s.length()/5.0);
-		String ss = "";
-		List<String> lis = new LinkedList<>();
-		for (int i =0;i<s.length();i++){
-		        ss+=s.charAt(i);
-		        if(ss.length()==div){
-		            lis.add(ss);
-		            ss="";
-                }
-        }
-		while (lis.size()<5){
-		    lis.add(ss);
-		    ss="";
+    public static Pattern pat = Pattern.compile("\\d+\\.?\\d");
+    public static String find(String town,String strng){
+        return Arrays.stream(strng.split("\n"))
+                .filter(s-> Arrays.stream(s.split(":")).toArray(String[]::new)[0].equals(town))
+                .toArray(String[]::new)[0];
+    }
+    public static List<Double> toList(String find){
+        List<Double> lis = new LinkedList<Double>();
+        Matcher m = pat.matcher(find);
+        while (m.find()){
+            lis.add(Double.valueOf(m.group()));
         }
         return lis;
     }
+    public static double mean(String town, String strng) {
+        // your code
+        System.out.println(town);
+        System.out.println(strng);
+        try {
+            String str = find(town,strng);
 
-    public static String  demovingShift(List<String> s, int shift) {
-        final int[] cont = {shift-1};
-        return Arrays.stream(s.stream().reduce("",(acc,curr)->acc+curr)
-                .split(""))
-                .map(w->{
-                        cont[0]++;
-                    if (upper.matcher(w).find()){
-                        return String.valueOf((char) ((w.charAt(0)-'A'-cont[0]+('Z'-'A'+1)*100)%('Z'-'A'+1)+'A'));
-                    }
-                    else if(lower.matcher(w).find()){
-                        return String.valueOf((char) ((w.charAt(0)-'a'-cont[0]+('z'-'a'+1)*100)%('z'-'a'+1)+'a'));
-                    }
-                    return w;
-                })
-                .collect(Collectors.joining());
+            List<Double> lis = toList(str);
+            return lis.stream().reduce(0.0,(acc,curr)->acc+curr)/lis.size();
+        }catch (Exception err){
+            return -1;
+        }
+
+    }
+    public static double variance(String town, String strng) {
+        // your code
+        System.out.println(town);
+        System.out.println(strng);
+        try {
+            String str = find(town,strng);
+
+            List<Double> lis = toList(str);
+            double mean = lis.stream().reduce(0.0,(acc,curr)->acc+curr)/lis.size();
+            return lis.stream().reduce(0.0,(acc,curr)->acc+Math.pow(curr-mean,2))/lis.size();
+        }catch (Exception ex){
+            return -1;
+        }
+
     }
 
 
+
     public static void main(String[] args) {
-        movingShift("I should have known that you would have a perfect answer for me!!!",1);
-        String word = demovingShift(Arrays.asList("J vltasl rlhr ", "zdfog odxr ypw", " atasl rlhr p ", "gwkzzyq zntyhv", " lvz wp!!!"),1);
+        String data =
+                "Rome:Jan 81.2,Feb 63.2,Mar 70.3,Apr 55.7,May 53.0,Jun 36.4,Jul 17.5,Aug 27.5,Sep 60.9,Oct 117.7,Nov 111.0,Dec 97.9" +
+                        "\n" +
+                        "London:Jan 48.0,Feb 38.9,Mar 39.9,Apr 42.2,May 47.3,Jun 52.1,Jul 59.5,Aug 57.2,Sep 55.4,Oct 62.0,Nov 59.0,Dec 52.9" +
+                        "\n" +
+                        "Paris:Jan 182.3,Feb 120.6,Mar 158.1,Apr 204.9,May 323.1,Jun 300.5,Jul 236.8,Aug 192.9,Sep 66.3,Oct 63.3,Nov 83.2,Dec 154.7" +
+                        "\n" +
+                        "NY:Jan 108.7,Feb 101.8,Mar 131.9,Apr 93.5,May 98.8,Jun 93.6,Jul 102.2,Aug 131.8,Sep 92.0,Oct 82.3,Nov 107.8,Dec 94.2" +
+                        "\n" +
+                        "Vancouver:Jan 145.7,Feb 121.4,Mar 102.3,Apr 69.2,May 55.8,Jun 47.1,Jul 31.3,Aug 37.0,Sep 59.6,Oct 116.3,Nov 154.6,Dec 171.5" +
+                        "\n" +
+                        "Sydney:Jan 103.4,Feb 111.0,Mar 131.3,Apr 129.7,May 123.0,Jun 129.2,Jul 102.8,Aug 80.3,Sep 69.3,Oct 82.6,Nov 81.4,Dec 78.2" +
+                        "\n" +
+                        "Bangkok:Jan 10.6,Feb 28.2,Mar 30.7,Apr 71.8,May 189.4,Jun 151.7,Jul 158.2,Aug 187.0,Sep 319.9,Oct 230.8,Nov 57.3,Dec 9.4" +
+                        "\n" +
+                        "Tokyo:Jan 49.9,Feb 71.5,Mar 106.4,Apr 129.2,May 144.0,Jun 176.0,Jul 135.6,Aug 148.5,Sep 216.4,Oct 194.1,Nov 95.6,Dec 54.4" +
+                        "\n" +
+                        "Beijing:Jan 3.9,Feb 4.7,Mar 8.2,Apr 18.4,May 33.0,Jun 78.1,Jul 224.3,Aug 170.0,Sep 58.4,Oct 18.0,Nov 9.3,Dec 2.7" +
+                        "\n" +
+                        "Lima:Jan 1.2,Feb 0.9,Mar 0.7,Apr 0.4,May 0.6,Jun 1.8,Jul 4.4,Aug 3.1,Sep 3.3,Oct 1.7,Nov 0.5,Dec 0.7";
+        double val = variance("London", data);
         System.out.println();
     }
 
